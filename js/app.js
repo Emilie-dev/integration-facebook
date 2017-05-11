3de8cb4e70c5b51c00efb0512ad3765c560cda72
Chauvin <chauvin-e@orange.fr>
2017-05-11 17:03:17 +0200

    add jade, init hapi

---
+++ js/app.js
@@ -0,0 +1,1 @@
+ validator.isEmail('foo@bar.com');
--- server.js
+++ server.js
@@ -1,19 +1,28 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser'); 
+var nodefs = require('fs');
 
-app.listen(3000);
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/'));
 
-
-
-
-app.get('/', function (req, res){
+app.get('/registration', function (req, res){
 	res.sendFile(__dirname + '/index.html');
-})
+});
 
+/*app.post('/', function (req, res) {
+	res.sendFile(__dirname + '/views');
+});*/
 
-app.post('/registration', function (req, res) {
-	res.send("yop");
-})
+//ajout de jade.js
+app.set('views', './views');
+app.set('view engine', 'jade');
 
-app.use(express.static('form-facebook'));
+app.post('/auth', function (req, res) {
+	res.render('auth', {user: req.body});
+});
 
+app.listen(3000, function () {
+	console.log("PORT 3000");
+});
